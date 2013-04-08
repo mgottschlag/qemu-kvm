f3a03a88490f67d2b62a9901a82bccd961bc0a09
Mathias Gottschlag <mgottschlag@gmail.com>
2013-04-08 12:11:45 +0200

    Splitted migration code into a different file.

--- hw/pscnv_virt.h
+++ hw/pscnv_virt.h
@@ -1,2 +1,202 @@
 
+#ifndef PSCNV_VIRT_H_INCLUDED
+#define PSCNV_VIRT_H_INCLUDED
+
+#include "pci.h"
+#include "exec-memory.h"
+#include "vmstate.h"
+
+#define PAGE_SHIFT 12
+#define PAGE_SIZE (1 << PAGE_SHIFT)
+#define PAGE_MASK (PAGE_SIZE - 1)
+
+//#define PSCNV_DEBUG_IO
+
+#define PSCNV_VIRT_MMIO_SIZE 0x1000
+#define PSCNV_CALL_SLOT_SIZE 0x100
+#define PSCNV_CALL_SLOT_COUNT 32
+#define PSCNV_CALL_AREA_SIZE (PSCNV_CALL_SLOT_COUNT * PSCNV_CALL_SLOT_SIZE)
+#define PSCNV_VIRT_VRAM_SIZE 0x10000000
+
+#define PSCNV_VIRT_CHAN_COUNT 128
+
+#define EXECUTE_CALL_REG 0x0
+#define PTIMER_TIME_REG 0x4
+#define GPU_INFO_REG_BASE 0x8
+
+#define PSCNV_INFO_PCI_VENDOR   0
+#define PSCNV_INFO_PCI_DEVICE   1
+#define PSCNV_INFO_BUS_TYPE     2
+#define PSCNV_INFO_CHIPSET_ID   3
+#define PSCNV_INFO_GRAPH_UNITS  4
+#define PSCNV_INFO_GPC_COUNT    5
+#define PSCNV_INFO_TP_COUNT_IDX 6
+#define PSCNV_INFO_MP_COUNT     7
+#define PSCNV_INFO_COUNT        8
+
+#define PSCNV_CMD_GET_PARAM 1
+#define PSCNV_CMD_MEM_ALLOC 2
+#define PSCNV_CMD_MAP 3
+#define PSCNV_CMD_MEM_FREE 4
+#define PSCNV_CMD_VSPACE_ALLOC 5
+#define PSCNV_CMD_VSPACE_FREE 6
+#define PSCNV_CMD_CHAN_NEW 7
+#define PSCNV_CMD_CHAN_FREE 8
+#define PSCNV_CMD_VSPACE_MAP 9
+#define PSCNV_CMD_VSPACE_UNMAP 10
+#define PSCNV_CMD_OBJ_VDMA_NEW 11
+#define PSCNV_CMD_OBJ_ENG_NEW 12
+#define PSCNV_CMD_FIFO_INIT 13
+#define PSCNV_CMD_FIFO_INIT_IB 14
+
+#define PSCNV_RESULT_NO_ERROR 0x80000000
+#define PSCNV_RESULT_UNKNOWN_CMD 0x80000001
+#define PSCNV_RESULT_ERROR 0x80000002
+
+#define DUMP_HYPERCALLS
+/*#define PSCNV_DEBUG_CHAN_ACCESS
+#define PSCNV_DEBUG_VRAM_ACCESS*/
+
+struct pscnv_memory_area {
+    uint32_t start;
+    uint32_t size;
+    struct pscnv_memory_area *next;
+    struct pscnv_memory_area *prev;
+    uint32_t handle;
+};
+
+struct pscnv_memory_allocation {
+    uint64_t size;
+    uint64_t map_handle;
+    uint32_t handle;
+    uint32_t next;
+    uint32_t cid;
+    struct pscnv_memory_area *mapping;
+    void *migration_mapping;
+};
+
+struct pscnv_alloc_mem_cmd {
+    uint32_t command;
+    uint32_t flags;
+    uint64_t size;
+    uint32_t tile_flags;
+    uint32_t cookie;
+    uint32_t handle;
+};
+
+struct pscnv_free_mem_cmd {
+    uint32_t command;
+    uint32_t handle;
+};
+
+struct pscnv_map_cmd {
+    uint32_t command;
+    uint32_t handle;
+    uint32_t start;
+};
+
+struct pscnv_vspace_cmd {
+    uint32_t command;
+    uint32_t vid;
+};
+
+struct pscnv_chan_new_cmd {
+    uint32_t command;
+    uint32_t vid;
+    uint32_t cid;
+};
+
+struct pscnv_chan_free_cmd {
+    uint32_t command;
+    uint32_t cid;
+};
+
+struct pscnv_vspace_map_cmd {
+    uint32_t command;
+    uint32_t vid;
+    uint32_t handle;
+    uint32_t back;
+    uint64_t start;
+    uint64_t end;
+    uint64_t offset;
+    uint32_t flags;
+};
+
+struct pscnv_vspace_unmap_cmd {
+    uint32_t command;
+    uint32_t vid;
+    uint64_t offset;
+};
+
+struct pscnv_obj_vdma_new_cmd {
+    uint32_t command;
+    uint32_t cid;
+    uint32_t handle;
+    uint32_t oclass;
+    uint64_t start;
+    uint64_t size;
+    uint32_t flags;
+    int32_t ret;
+};
+
+struct pscnv_fifo_init_cmd {
+    uint32_t command;
+    uint32_t cid;
+    uint32_t pb_handle;
+    uint32_t flags;
+    uint64_t pb_start;
+    uint32_t slimask;
+    int32_t ret;
+};
+
+struct pscnv_fifo_init_ib_cmd {
+    uint32_t command;
+    uint32_t cid;
+    uint32_t pb_handle;
+    uint32_t flags;
+    uint64_t ib_start;
+    uint32_t slimask;
+    uint32_t ib_order;
+    int32_t ret;
+};
+
+struct pscnv_obj_eng_new_cmd {
+    uint32_t command;
+    uint32_t cid;
+    uint32_t handle;
+    uint32_t oclass;
+    uint32_t flags;
+    int32_t ret;
+};
+
+typedef struct {
+    PCIDevice pci_dev;
+    MemoryRegion mmio_bar;
+    MemoryRegion call_area_bar;
+    MemoryRegion vram_bar;
+    unsigned int chan_bar_size;
+    MemoryRegion chan_bar;
+    char *call_area_memory;
+    char *vram_bar_memory;
+    char *chan_bar_memory;
+    uint32_t chan_handle[PSCNV_VIRT_CHAN_COUNT];
+    uint32_t chan_vspace[PSCNV_VIRT_CHAN_COUNT];
+    struct pscnv_fifo_init_ib_cmd fifo_init[PSCNV_VIRT_CHAN_COUNT];
+    void *channel_content;
+
+    int drm_fd;
+    uint32_t gpu_info[PSCNV_INFO_COUNT];
+
+    struct pscnv_memory_allocation *alloc_data;
+    uint32_t alloc_count;
+    uint32_t alloc_freelist;
+
+    struct pscnv_memory_area *memory_areas;
+
+    int is_nv50;
+} PscnvState;
+
+extern SaveVMHandlers pscnv_save_handlers;
+
+#endif
 
---
+++ hw/pscnv_virt_migration.c
@@ -0,0 +1,291 @@
+
+#include "pscnv_virt.h"
+
+#include <libpscnv.h>
+#include <sys/mman.h>
+
+/**
+ * Checks whether an entry in the chan memory region is associated with a
+ * pscnv channel.
+ */
+static int chan_is_allocated(PscnvState *d, uint32_t chan) {
+    return d->chan_handle[chan] != (uint32_t)-1;
+}
+
+static void pscnv_resume_channels(PscnvState *d) {
+    int ret;
+    int i;
+    char *current_channel;
+    unsigned int chsize;
+
+    /**
+     * Reallocate all channels.
+     */
+    chsize = d->is_nv50 ? 0x2000 : 0x1000;
+    current_channel = d->channel_content;
+    for (i = 0; i < PSCNV_VIRT_CHAN_COUNT; i++) {
+        if (chan_is_allocated(d, i)) {
+            fprintf(stderr, "pscnv_save_cancel: Allocating channel %d.\n", i);
+            uint64_t map_handle;
+            void *chmem;
+            /* create a new channel */
+            ret = pscnv_chan_new(d->drm_fd, d->chan_vspace[i],
+                                 &d->chan_handle[i], &map_handle);
+            if (ret) {
+                fprintf(stderr, "pscnv_virt: pscnv_chan_new failed (%d)\n", ret);
+            }
+            if (d->chan_handle[i] >= PSCNV_VIRT_CHAN_COUNT) {
+                fprintf(stderr, "pscnv_virt: Bug: invalid cid %d\n",
+                        d->chan_handle[i]);
+                return;
+            }
+            /* map the channel */
+            chmem = mmap(d->chan_bar_memory + i * chsize, chsize,
+                         PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED,
+                         d->drm_fd, map_handle);
+            if (chmem == MAP_FAILED) {
+                fprintf(stderr, "pscnv_virt: Could not map channel %d\n",
+                        d->chan_handle[i]);
+            }
+            /* restore the channel state */
+            memcpy(d->chan_bar_memory + i * chsize, current_channel, chsize);
+            if (d->fifo_init[i].command != (uint32_t)-1) {
+                struct pscnv_fifo_init_ib_cmd *cmd = &d->fifo_init[i];
+                ret = pscnv_fifo_init_ib(d->drm_fd, d->chan_handle[i], cmd->pb_handle,
+                                         cmd->flags, cmd->slimask, cmd->ib_start,
+                                         cmd->ib_order);
+                if (ret) {
+                    fprintf(stderr, "pscnv_virt: pscnv_fifo_init_ib failed (%d)\n", ret);
+                }
+            }
+            current_channel += chsize;
+        }
+    }
+}
+
+static void pscnv_revert_migration_mappings(PscnvState *d) {
+    int i;
+    void *mmap_result;
+
+    // Copy changed memory content from RAM to GPU
+    for (i = 0; i < d->alloc_count; i++) {
+        struct pscnv_memory_allocation *obj = &d->alloc_data[i];
+        if (obj->size != (uint64_t)-1) {
+            if (obj->migration_mapping != NULL && obj->mapping != NULL) {
+                memcpy(d->vram_bar_memory + obj->mapping->start,
+                       obj->migration_mapping, obj->size);
+            }
+        }
+    }
+    // Unmap copies made for migration
+    mmap_result = mmap(d->vram_bar_memory, PSCNV_VIRT_VRAM_SIZE, PROT_NONE,
+                       MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    if (mmap_result == MAP_FAILED) {
+        fprintf(stderr, "pscnv_virt: could not unmap vram\n");
+    }
+    // Map objects into the VRAM BAR again
+    for (i = 0; i < d->alloc_count; i++) {
+        struct pscnv_memory_allocation *obj = &d->alloc_data[i];
+        if (obj->size != (uint64_t)-1) {
+            if (obj->migration_mapping != NULL) {
+                if (munmap(obj->migration_mapping, obj->size) != 0) {
+                    fprintf(stderr, "pscnv_virt: could not unmap obj %d\n", i);
+                }
+            }
+            obj->migration_mapping = NULL;
+            if (obj->mapping != NULL) {
+                mmap_result = mmap(d->vram_bar_memory + obj->mapping->start, obj->size,
+                        PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, d->drm_fd,
+                        obj->map_handle);
+                if (mmap_result == MAP_FAILED) {
+                    fprintf(stderr, "pscnv_virt: could not map obj %d\n", i);
+                }
+            }
+        }
+    }
+}
+
+static void pscnv_save_state(QEMUFile *f, void *opaque) {
+    fprintf(stderr, "pscnv_save_state\n");
+    // TODO
+}
+static int pscnv_save_live_setup(QEMUFile *f, void *opaque) {
+    int i;
+    unsigned int chsize;
+    PscnvState *d = opaque;
+    unsigned int channel_count;
+    char *current_channel;
+    void *mmap_result;
+    int ret;
+
+    fprintf(stderr, "pscnv_save_live_setup\n");
+
+    /**
+     * Idle all channels.
+     */
+    channel_count = 0;
+    chsize = d->is_nv50 ? 0x2000 : 0x1000;
+    for (i = 0; i < PSCNV_VIRT_CHAN_COUNT; i++) {
+        if (chan_is_allocated(d, i)) {
+            uint32_t *channel = (uint32_t*)(d->chan_bar_memory + i * chsize);
+            fprintf(stderr, "dma put: %08x get: %08x\n"
+                            "ib put: %08x get: %08x\n",
+                    channel[0x10], channel[0x11], channel[0x22], channel[0x23]);
+            while (channel[0x10] != channel[0x11] || channel[0x22] != channel[0x23]) {
+                // busy-loop until the channel is idle
+            }
+            channel_count++;
+        }
+    }
+
+    /**
+     * Save the channel content and unmap the channels.
+     */
+    d->channel_content = malloc(chsize * channel_count);
+    current_channel = d->channel_content;
+    for (i = 0; i < PSCNV_VIRT_CHAN_COUNT; i++) {
+        if (chan_is_allocated(d, i)) {
+            fprintf(stderr, "pscnv_save_cancel: Deleting channel %d.\n", i);
+            memcpy(current_channel, d->chan_bar_memory + i * chsize, chsize);
+            /* delete the channel */
+            ret = pscnv_chan_free(d->drm_fd, d->chan_handle[i]);
+            if (ret) {
+                fprintf(stderr, "pscnv_virt: could not free channel (%d)\n", ret);
+            }
+            /* unmap the channel */
+            mmap_result = mmap(d->chan_bar_memory + i * chsize, chsize,
+                    PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+            if (mmap_result == MAP_FAILED) {
+                fprintf(stderr, "pscnv_virt: could not unmap channel %d\n", i);
+            }
+            current_channel += chsize;
+        }
+    }
+    mmap_result = mmap(d->chan_bar_memory, d->chan_bar_size,
+                       PROT_READ | PROT_WRITE,
+                       MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    if (mmap_result == MAP_FAILED) {
+        fprintf(stderr, "pscnv_virt: could not unmap channels\n");
+        return -1;
+    }
+
+    /**
+     * Map GPU memory contents.
+     */
+    // TODO: DMA
+    /*for (i = 0; i < d->alloc_count; i++) {
+        struct pscnv_memory_allocation *obj = &d->alloc_data[i];
+        if (obj->size != (uint64_t)-1) {
+            if (obj->mapping != NULL) {
+                obj->migration_mapping = NULL;
+            } else {
+                obj->migration_mapping =
+                        mmap(NULL, obj->size, PROT_READ | PROT_WRITE,
+                             MAP_SHARED, d->drm_fd, obj->map_handle);
+                if (obj->migration_mapping == MAP_FAILED) {
+                    fprintf(stderr, "pscnv_virt: could not map obj %d\n", i);
+                    return -1;
+                }
+            }
+        }
+    }*/
+    mmap_result = mmap(d->vram_bar_memory, PSCNV_VIRT_VRAM_SIZE,
+                       PROT_READ | PROT_WRITE,
+                       MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    if (mmap_result == MAP_FAILED) {
+        fprintf(stderr, "pscnv_virt: could not unmap vram\n");
+        return -1;
+    }
+    for (i = 0; i < d->alloc_count; i++) {
+        struct pscnv_memory_allocation *obj = &d->alloc_data[i];
+        if (obj->size != (uint64_t)-1) {
+            obj->migration_mapping =
+                    mmap(NULL, obj->size, PROT_READ | PROT_WRITE,
+                         MAP_SHARED, d->drm_fd, obj->map_handle);
+            if (obj->migration_mapping == MAP_FAILED) {
+                fprintf(stderr, "pscnv_virt: could not map obj %d\n", i);
+                return -1;
+            }
+            if (obj->mapping != NULL) {
+                memcpy(d->vram_bar_memory + obj->mapping->start,
+                       obj->migration_mapping, obj->size);
+            }
+        }
+    }
+
+    /**
+     * We just let the normal RAM migration code take care of mapped buffers.
+     */
+    /*memory_region_set_log(&d->vram_bar, 1, DIRTY_MEMORY_PSCNV);
+    memory_region_set_dirty(&d->vram_bar, 0, PSCNV_VIRT_VRAM_SIZE);
+    memory_region_sync_dirty_bitmap(&d->vram_bar);*/
+
+    // TODO
+
+    /**
+     * Start copying memory.
+     */
+    // TODO
+    return 0;
+}
+static int pscnv_save_live_iterate(QEMUFile *f, void *opaque) {
+    fprintf(stderr, "pscnv_save_live_iterate\n");
+    // TODO
+    return 1;
+}
+static int pscnv_save_live_complete(QEMUFile *f, void *opaque) {
+    int i;
+    PscnvState *d = opaque;
+
+    fprintf(stderr, "pscnv_save_live_complete\n");
+
+    //memory_region_sync_dirty_bitmap(&d->vram_bar);
+
+    /**
+     * Write memory into the buffer.
+     */
+    for (i = 0; i < d->alloc_count; i++) {
+        struct pscnv_memory_allocation *obj = &d->alloc_data[i];
+        void *data;
+        if (obj->size != (uint64_t)-1) {
+            /*if (obj->mapping != NULL) {
+                data = d->vram_bar_memory + obj->mapping->start;
+            } else {
+                data = obj->migration_mapping;
+            }*/
+            data = obj->migration_mapping;
+            qemu_put_be32(f, obj->size);
+            qemu_put_buffer(f, data, obj->size);
+        }
+    }
+
+    /**
+     * Revert all changes made by the migration functions.
+     */
+    pscnv_revert_migration_mappings(d);
+    pscnv_resume_channels(d);
+    return 0;
+}
+static void pscnv_save_cancel(void *opaque) {
+    PscnvState *d = opaque;
+
+    fprintf(stderr, "pscnv_save_cancel\n");
+
+    pscnv_revert_migration_mappings(d);
+    pscnv_resume_channels(d);
+}
+static int pscnv_load_state(QEMUFile *f, void *opaque, int version_id) {
+    fprintf(stderr, "pscnv_load_state\n");
+    // TODO
+    return -1;
+}
+
+SaveVMHandlers pscnv_save_handlers = {
+    .save_state = pscnv_save_state,
+    .save_live_setup = pscnv_save_live_setup,
+    .save_live_iterate = pscnv_save_live_iterate,
+    .save_live_complete = pscnv_save_live_complete,
+    .cancel = pscnv_save_cancel,
+    .load_state = pscnv_load_state,
+};
+
